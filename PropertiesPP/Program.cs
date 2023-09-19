af7fc99d9986c19f444a1663442cf17579ed66fd
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-19 19:20:52 +0530

    Add project files.

---
+++ PropertiesPP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyDemo
+{
+    public class PropertyDemo
+    {
+        private string _fristName = string.Empty;
+        private string _lastName = string.Empty;
+        private int _Age;
+        private string country = "India";
+
+        public string empfristName
+        {
+            set { _fristName = value; }
+            get { return _fristName; }
+
+        }
+        
+
+        public string CountryName
+        {
+            get { return country; }
+        }
+    }
+        class Program
+        {
+            static void Main(string[] arg)
+            {
+
+
+                PropertyDemo obj = new PropertyDemo();
+                obj.empfristName = "vandu";
+                Console.WriteLine("My frist" + obj.empfristName);
+            Console.WriteLine("my country" + obj.CountryName);
+                Console.ReadLine();
+            }
+        }
+    }
+
